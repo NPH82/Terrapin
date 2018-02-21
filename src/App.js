8bc0d0f08e10821b117923b91cb2eb2122ee8ff6
NPH82 <nich1982@gmail.com>
2018-02-21 17:05:12 -0500

    react framework

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Terrapin Nesting App</h1>
+        </header>
+        <p className="App-intro">
+          This going to be the landing page.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
