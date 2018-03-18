a407d046156a10a706b7efabb7e08c3cb96e318e
NPH82 <nich1982@gmail.com>
2018-03-17 21:23:48 -0400

    completed input component, created welcome page and create account page

--- src/components/Form/FormBtn.js
+++ src/components/Form/FormBtn.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 class FormBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert("this value was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <button
+        onSubmit={this.handleSubmit}
         className="btn btn-default"
-        style={{backgroundColor:"#637d8a", padding:"10px 0px", color:"#fff", width:"50%", fontSize:30, border:"10 #000"}}
-        onClick={this.props.handleClick}>
+        style={{backgroundColor:"#637d8a", padding:"10px 0px", color:"#fff", width:"81%", fontSize:30, border:"1px solid #000", borderRadius:"3px"}}
+      >
         {this.props.label}
       </button>
     );
