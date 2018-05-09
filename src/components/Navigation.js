4258461bf8ca4ef1e5f607af7852eab1bc5a0bd2
NPH82 <nich1982@gmail.com>
2018-05-09 19:55:51 -0400

    creating constants file and routes for Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as routes from '../constants/routes';
+
+const Navigation = () =>
+  <div>
+    <ul>
+      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+      <li><Link to={routes.CREATE_ACCOUNT}>Create Account</Link></li>
+      <li><Link to={routes.ADMIN_HOME}>Administration</Link></li>
+      <li><Link to={routes.NESTING}>Nesting Form</Link></li>
+    </ul>
+  </div>
+
